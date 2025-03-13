fcc55f4595dcc44918345eca28d771dcad3c4ea1
Jc <lopesjean81@gmail.com>
2025-03-13 19:47:36 -0300

    :sparkes: feat: add auth plugin

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import {
   validatorCompiler,
 } from "fastify-type-provider-zod";
 
+import jwt from "@fastify/jwt";
 import { fastifySwagger } from "@fastify/swagger";
 import { fastifySwaggerUi } from "@fastify/swagger-ui";
 import { env } from "./env";
@@ -13,15 +14,25 @@ import { routes } from "./routes";
 
 const app = fastify().withTypeProvider();
 
+app.register(jwt, {
+  secret: env.JWT_SECRET,
+});
+
+app.decorate("authenticate", async (request, reply) => {
+  try {
+    await request.jwtVerify();
+  } catch (err) {
+    return reply
+      .status(401)
+      .send({ message: "Token inválido ou não fornecido" });
+  }
+});
+
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
 app.register(fastifyCors, {
   origin: "*",
-});
-
-app.register(require("@fastify/jwt"), {
-  secret: env.JWT_SECRET,
 });
 
 app.register(fastifySwagger, {
